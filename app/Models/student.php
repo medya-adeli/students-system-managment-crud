fb561cbbf9f95b005da914c9f43844d685175f96
adolf <midiaadeli12@gmail.com>
2022-09-03 02:51:19 +0430

    First comit

---
+++ app/Models/student.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class student extends Model
+{
+    use HasFactory;
+    protected $table='students';
+    protected $primarykey='id';
+    protected $fillable=['name','e-mail','phone'];
+}
---
+++ resources/views/students/index.blade.php
@@ -0,0 +1,4 @@
+@extends('students.layout')
+@section('content')
+    
+@endsection
