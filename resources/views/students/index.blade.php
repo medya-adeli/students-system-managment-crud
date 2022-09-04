5f8957fb0597e96cb34c68fc0fb412b7b8e45e01
adolf <midiaadeli12@gmail.com>
2022-09-04 12:11:19 +0430

    push

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -35,8 +35,8 @@
                 </tbody>
             </table>
             <div class="text-center">
-                 <button  type="button" class=" btn btn-success "><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="white" class="bi bi-person-plus-fill" viewBox="0 0 16 16">
+                 <button  type="button" class=" btn btn-success "><svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="white" class="bi bi-person-plus-fill" viewBox="0 0 16 16">
                     <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
                     <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>
-                  </svg><a class="text-white" href="{{url('/students/create')}}"> add Students</a></button>
+                  </svg><a class="text-white text-decoration-none" href="{{url('/students/create')}}"> add Students</a></button>
             </div>
