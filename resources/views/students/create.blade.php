3e0b21731fef16eda104ff1dfb469244ce52e31d
adolf <midiaadeli12@gmail.com>
2022-09-06 12:39:32 +0430

    push

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -4,7 +4,7 @@
     <div class="card-header">Students Page</div>
     <div class="card-body">
         
-        <form action="{{ url('students') }}" method="post" enctype="multipart/form-data">
+        <form action="{{ url('students') }}" method="post" autocomplete="off" enctype="multipart/form-data">
           {!! csrf_field() !!}
           <label>Name</label></br>
           <input type="text" name="name" id="name" class="form-control"></br>
@@ -13,7 +13,7 @@
           <label>phone</label></br>
           <input type="tel" name="phone" id="phone" minlength="11" maxlength="11" class="form-control"></br>
           <label>file</label></br>
-          <input type="file" name="image" id="image" class="form-control"></br>
+          <input type="file" name="image" id="image"  accept="image/*" class="form-control"></br>
           <input type="submit" value="Save" class="btn btn-success"></br>
 
       </form>
